5a2dad047790c693893d35df7d786089bf4a8a7a
ZaytsevaNA <natalie.zaytseva5676@gmail.com>
2023-10-23 21:57:20 +0300

    Лекция 4.3. Цикл: перебор элементов коллекции

--- java_for_testers/addressbook_web_tests/src/test/java/manader/GroupHelper.java
+++ java_for_testers/addressbook_web_tests/src/test/java/manader/GroupHelper.java
@@ -26,7 +26,7 @@ public class GroupHelper extends HelperBase {
     public void removeGroup() {
         openGroupPage();
         selectGroup();
-        removeSelectedGroup();
+        removeSelectedGroups();
         returnToGroupsPage();
     }
 
@@ -47,7 +47,7 @@ public class GroupHelper extends HelperBase {
         click(By.name("new"));
     }
 
-    private void removeSelectedGroup() {
+    private void removeSelectedGroups() {
         click(By.name("delete"));
     }
 
@@ -76,5 +76,18 @@ public class GroupHelper extends HelperBase {
     public int getCount() {
         openGroupPage();
         return manager.driver.findElements(By.name("selected[]")).size();
+    }
+
+    public void removeAllGroups() {
+        openGroupPage();
+        selectAllGroups();
+        removeSelectedGroups();
+    }
+
+    private void selectAllGroups() {
+        var checkboxes = manager.driver.findElements(By.name("selected[]"));
+        for (var checkbox : checkboxes) {
+            checkbox.click();
+        }
     }
 }
--- java_for_testers/addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ java_for_testers/addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -16,4 +16,13 @@ public class GroupRemovalTests extends TestBase {
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount - 1, newGroupCount);
     }
+
+    @Test
+    public void canRemoveAllGroupsAtOnce() {
+        if (app.groups().getCount() == 0) {
+            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+        }
+        app.groups().removeAllGroups();
+        Assertions.assertEquals(0, app.groups().getCount());
+    }
 }
