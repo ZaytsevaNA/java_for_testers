985badc37be2aa61afdbd7c6ce145a7d3142a26f
ZaytsevaNA <natalie.zaytseva5676@gmail.com>
2023-10-23 21:16:11 +0300

    Лекция 4.1. Коллекции: наборы элементов

---
+++ java_for_testers/sandbox/src/test/java/collections/CollectionTests.java
@@ -0,0 +1,30 @@
+package collections;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionTests {
+
+    @Test
+    void arrayTests() {
+        var array = new String[]{"a", "b", "c"};
+        Assertions.assertEquals(3, array.length);
+        Assertions.assertEquals("a", array[0]);
+
+        array[0] = "b";
+        Assertions.assertEquals("b", array[0]);
+    }
+
+    @Test
+    void listTests() {
+        var list = new ArrayList<>(List.of("a", "b", "c"));
+        Assertions.assertEquals(3, list.size());
+        Assertions.assertEquals("a", list.get(0));
+
+        list.set(0, "d");
+        Assertions.assertEquals("d", list.get(0));
+    }
+}
